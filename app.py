a2ff7d4afebd09ed63efdc9c2aed09f0368ced69
Kwasi Bronie <ojmanuel360@gmail.com>
2019-07-17 11:38:37 +0100

    Add heroku config

--- app.py
+++ app.py
@@ -1,34 +1,27 @@
 from flask import Flask, jsonify
-
+import os
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        Hello World
-    </body>
-    </html>
-    '''
+	 return 'Hello World'
 
 number = 0
 
 @app.route('/ping')
 def ping():
-    global number
-    number += 1
-    return 'you are pong ' + str(number)
+	 global number
+	 number += 1
+	 return 'you are pong' + str(number)
 
 @app.route('/api')
 def api():
-    return jsonify({"hello": "there"})
+        return jsonify({"hello": "there"})
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if (os.getenv('LIVE', 'false') == 'true'):
+        if __name__ == '__main__':
+		 app.run(debug=True, host='0.0.0.0')
+else:
+        if __name__ == '__main__':
+		 app.run(debug=False, host='0.0.0.0')
